ed0561dcf67e6c2c5dcd56604e5d36ff4ff0282d
최덕경 <deokgyeong.choi@opengallery.co.kr>
2020-05-26 23:47:40 +0900

    7

---
+++ algorithm_practice_kit/heap/3.py
@@ -0,0 +1,31 @@
+import heapq
+
+def solution(jobs):
+	N = len(jobs)
+	jobs = [[job[1], job[0]] for job in jobs]
+	jobs.sort(key=lambda x: x[1])
+
+	idx = 0
+	start = jobs[0][1]
+	total_wait_time = 0
+	job_cnt = 0
+
+	h = []
+
+	while job_cnt < N:
+		while idx < N and jobs[idx][1] <= start:
+			heapq.heappush(h, jobs[idx])
+			idx += 1
+
+		if not h:
+			start = jobs[idx][1]
+			continue
+
+		min_job = heapq.heappop(h)
+
+		start += min_job[0]
+		total_wait_time += start - min_job[1]
+		job_cnt += 1
+
+	answer = total_wait_time // N
+	return answer
