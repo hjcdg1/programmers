5fb6c2e9549582f1159ee3acba69b15729bc17e4
hjcdg1 <deokgyeong.choi@opengallery.co.kr>
2020-07-03 09:32:01 +0900

    12

--- algorithm_practice_kit/heap/4.py
+++ algorithm_practice_kit/heap/4.py
@@ -1,31 +1,34 @@
 import heapq
 
 def solution(operations):
-	max_h = []
-	min_h = []
+    max_h = []
+    min_h = []
 
-	size = 0
-	for operation in operations:
-		op, data = tuple(operation.split())
-		data = int(data)
+    is_deleted = [False for _ in range(1000000)]
 
-		if op == 'I':
-			heapq.heappush(max_h, -data)
-			heapq.heappush(min_h, data)
-			size += 1
-		else:
-			if size > 0:
-				if data == 1:
-					heapq.heappop(max_h)
-				else:
-					heapq.heappop(min_h)
-				size -= 1
+    for idx, operation in enumerate(operations):
+        op, data = tuple(operation.split())
+        data = int(data)
 
-			if size == 0:
-				max_h = []
-				min_h = []
+        if op == 'I':
+            heapq.heappush(max_h, (-data, idx))
+            heapq.heappush(min_h, (data, idx))
+        else:
+            if not (max_h and min_h):
+                continue
 
-	if size == 0:
-		return [0, 0]
-	else:
-		return [-heapq.heappop(max_h), heapq.heappop(min_h)]
+            if data == 1:
+                is_deleted[heapq.heappop(max_h)[1]] = True
+            else:
+                is_deleted[heapq.heappop(min_h)[1]] = True
+
+            while max_h and is_deleted[max_h[0][1]]:
+                heapq.heappop(max_h)
+
+            while min_h and is_deleted[min_h[0][1]]:
+                heapq.heappop(min_h)
+
+    if not (max_h and min_h):
+        return [0, 0]
+    else:
+        return [-heapq.heappop(max_h)[0], heapq.heappop(min_h)[0]]
